190390529a2a2260765732fe93c1d430e7b35cf5
lexnicholls19 <nicholls.aom@gmail.com>
2026-06-21 01:15:06 -0500

    Update build.gradle.kts
    build: bump version to 1.2.0 (code 3)

--- LoveCounter/app/build.gradle.kts
+++ LoveCounter/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.lexnicholls.lovecounter"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
