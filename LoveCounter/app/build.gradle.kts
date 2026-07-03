6a3dc17ef8df540e6c97508fa6cee02f5f284405
lexnicholls19 <nicholls.aom@gmail.com>
2026-07-03 13:45:20 -0500

    bumped internal version to 2.2.0

--- LoveCounter/app/build.gradle.kts
+++ LoveCounter/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.lexnicholls.lovecounter"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "2.0.0"
+        versionCode = 7
+        versionName = "2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
